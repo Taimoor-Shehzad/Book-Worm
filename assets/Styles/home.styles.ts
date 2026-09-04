f6e39d4ecd7ac66ec07f1315533aa2b3ea9003a8
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-04 12:51:53 +0500

    Homepage UI completed

--- assets/Styles/home.styles.ts
+++ assets/Styles/home.styles.ts
@@ -5,6 +5,93 @@ const CreateHomeStyles = () => {
   const styles = StyleSheet.create({
     container: {
       backgroundColor: COLORS.background,
+      padding: 22,
+      paddingVertical: 0,
+      flexGrow: 1,
+    },
+
+    listContainer: {
+      gap: 18,
+      flexGrow: 1,
+      padding: 3,
+      paddingVertical: 9,
+    },
+
+    bookContainer: {
+      backgroundColor: COLORS.white,
+      width: "100%",
+      padding: 12,
+      elevation: 3,
+      borderRadius: 14,
+      gap: 12,
+    },
+
+    bookHeaderContainer: {
+      flexDirection: "row",
+      flexGrow: 1,
+      gap: 8,
+      alignItems: "center",
+    },
+
+    profileImage: {
+      height: 28,
+      width: 28,
+    },
+
+    userName: {
+      color: COLORS.textPrimary,
+      fontWeight: 800,
+    },
+
+    bookImageContanier: {
+      width: "100%",
+      height: 170,
+    },
+
+    bookImage: {
+      height: "100%",
+      width: "100%",
+      borderRadius: 14,
+    },
+
+    bookDetails: { gap: 5 },
+
+    bookTitle: {
+      color: COLORS.textPrimary,
+      fontSize: 15,
+      fontWeight: "bold",
+    },
+
+    bookRating: {
+      flexDirection: "row",
+      gap: 2,
+    },
+
+    bookCaption: {
+      color: COLORS.textPrimary,
+    },
+
+    bookDate: {
+      color: COLORS.textSecondary,
+      fontSize: 12,
+    },
+
+    header: {
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 30,
+      gap: 3,
+    },
+
+    headerTitle: {
+      color: COLORS.primary,
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+
+    headerCaption: {
+      color: COLORS.placeholderText,
+      fontSize: 14,
     },
   });
 
--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,19 +1,103 @@
-import { View, Text } from "react-native";
-import { Pressable } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
+// import { Image } from "expo-image";
 import { useAuthStore } from "../../../store/authStore";
+import CreateHomeStyles from "@/assets/Styles/home.styles";
+import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../../constants/colors";
 
 const Home = () => {
   const { logout } = useAuthStore();
+  const [books, setBooks] = useState([
+    {
+      _id: "12313",
+      work: "hhh",
+      num: 7377,
+    },
+    {
+      _id: "342424",
+      work: "hhh",
+      num: 7787,
+    },
+    {
+      _id: "90898",
+      work: "hhh",
+      num: 77,
+    },
+  ]); //PH
+
+  const styles = CreateHomeStyles();
+  const fetchBooks = async () => {};
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const renderBook = ({ item }: { item: any }) => (
+    //PH DATA
+    <View style={styles.bookContainer}>
+      <View style={styles.bookHeaderContainer}>
+        <Image
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2yGDr34mjFWlbLRGMxLIhAoPCC5vmMvDEbVm6cSZLeNw-n0eWNj-2wls&s",
+          }}
+          style={styles.profileImage}
+          resizeMode="contain"
+        />
+        <Text style={styles.userName}>John Doe</Text>
+      </View>
+      <View style={styles.bookImageContanier}>
+        <Image
+          style={styles.bookImage}
+          resizeMode="cover"
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr88sLqEZYYxTyexHT4zhukQ28FXmsQio4MJy0-tLOrA&s=10",
+          }}
+        />
+      </View>
+      <View style={styles.bookDetails}>
+        <Text style={styles.bookTitle}>The Hunger Games</Text>
+        <View style={styles.bookRating}>{renderRating(3)}</View>
+        <Text style={styles.bookCaption}>
+          A tale of survival rebellion and sacrifice for family.
+        </Text>
+        <Text style={styles.bookDate}>3/9/2025</Text>
+      </View>
+    </View>
+  );
+
+  const renderRating = (rating: any) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <Ionicons
+          key={i}
+          size={14}
+          name={i <= rating ? "star" : "star-outline"}
+          color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+        />,
+      );
+    }
+    return stars;
+  };
+
   return (
-    <View>
-      <Text>Welcome Home</Text>
-      <Pressable
-        onPress={() => {
-          logout();
-        }}
-      >
-        <Text>Logout</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <FlatList
+        data={books}
+        renderItem={renderBook}
+        keyExtractor={(item) => item._id}
+        contentContainerStyle={styles.listContainer}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View style={styles.header}>
+            <Text style={styles.headerTitle}>BookWorm 🐌</Text>
+            <Text style={styles.headerCaption}>
+              Discover great books from the community 👇
+            </Text>
+          </View>
+        }
+      />
     </View>
   );
 };
